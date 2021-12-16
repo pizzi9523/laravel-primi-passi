dee3057f3eb73eaf170eeb0f0df23f653ab40aa0
pizzi9523 <pizzitolagiuseppe@live.it>
2021-12-16 15:26:39 +0100

    complete page home, insert hello world and datas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'names' => ['Giuseppe', 'Fabio', 'Fabiola', 'Tiziano']
+    ];
+    return view('home', $data);
 });
